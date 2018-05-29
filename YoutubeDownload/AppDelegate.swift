371b0cbc85ba89b9a7f7820a80d249e6c9fd602f
Angelo Rodriguez <31600726+JMARyA@users.noreply.github.com>
2018-05-30 01:29:05 +0200

    Initial Commit

---
+++ YoutubeDownload/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  YoutubeDownload
+//
+//  Created by Angelo Rodriguez on 30.05.18.
+//  Copyright © 2018 Arya. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ YoutubeDownload/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YoutubeDownload
+//
+//  Created by Angelo Rodriguez on 30.05.18.
+//  Copyright © 2018 Arya. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
