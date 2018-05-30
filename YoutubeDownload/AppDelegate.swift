ff73072407ff23115b2f90e526a0b7e35451571f
Angelo Rodriguez <31600726+JMARyA@users.noreply.github.com>
2018-05-30 07:48:45 +0200

    Fixed an UI Issue

--- YoutubeDownload/AppDelegate.swift
+++ YoutubeDownload/AppDelegate.swift
@@ -11,10 +11,7 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -24,6 +21,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
-
+    
+    
+    
 }
 
--- YoutubeDownload/ViewController.swift
+++ YoutubeDownload/ViewController.swift
@@ -164,6 +164,7 @@ class ViewController: NSViewController, NSOpenSavePanelDelegate {
         return str
     }
     
+
     
     
 }
