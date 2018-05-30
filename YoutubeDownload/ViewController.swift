9e5315a233deb483583bc56a2e9fe95d3a9d9e98
Angelo Rodriguez <31600726+JMARyA@users.noreply.github.com>
2018-05-30 02:44:17 +0200

    Added GUI

--- YoutubeDownload/ViewController.swift
+++ YoutubeDownload/ViewController.swift
@@ -9,12 +9,26 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    @IBOutlet weak var audioFormatButton: NSPopUpButton!
+    @IBOutlet weak var videoFormatButton: NSPopUpButton!
+    @IBOutlet weak var qualityButton: NSPopUpButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+        
+        self.audioFormatButton.removeAllItems()
+        self.audioFormatButton.addItems(withTitles: ["Best Format", "AAC", "FLAC", "MP3", "M4A", "OPUS", "VORBIS", "WAV"])
+        self.videoFormatButton.removeAllItems()
+        self.videoFormatButton.addItems(withTitles: ["MP4", "FLV", "OGG", "WEBM", "MKV", "AVI"])
+        
+        self.qualityButton.removeAllItems()
+        self.qualityButton.addItems(withTitles: ["Best", "2160p", "1440p", "1080p", "720p", "420p"])
+        
+        
+        }
 
     override var representedObject: Any? {
         didSet {
